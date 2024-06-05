b87d33f576f98ce6d962a76641dbb3d41996ed2b
dynich <mrhap21d@student.unhas.ac.id>
2024-06-05 15:48:41 +0800

    feat: 1 welcome page

--- app/src/main/java/com/example/swaraapp/MainActivity.kt
+++ app/src/main/java/com/example/swaraapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.swaraApp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.swaraapp.R
 
@@ -9,17 +12,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+        val signUp = findViewById<TextView>(R.id.sign_up)
+        val rememberMe = findViewById<CheckBox>(R.id.remember_me)
+        val forgotPassword = findViewById<TextView>(R.id.forgot_password)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
 
         btnLogin.setOnClickListener {
             // Handle login button click
         }
 
-        btnSignUp.setOnClickListener {
-            // Handle sign up button click
+        signUp.setOnClickListener {
+            // Handle sign up text click
+        }
+
+        forgotPassword.setOnClickListener {
+            // Handle forgot password text click
         }
     }
 }
